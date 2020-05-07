f8928a72fe21f4b50cf17101d3812b9d9d72c454
Ayobami Akingbade <akinayosamtom@gmail.com>
2020-05-07 06:21:55 +0100

    Scrolling fix

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,20 +1,17 @@
 (function(){
-    // $('.pick-package').click(function(){
-    //     $('.pick-package').removeClass('picked')
-    //     $(this).addClass('picked')
-    //     $('#plan_id').val($(this).data('id'));
-    // });
-    // $('.choose-payment').click(function(){
-    //     $('.choose-payment').removeClass('picked')
-    //     $(this).addClass('picked')
-    //     $('#method').val($(this).data('method')); 
-    // });
-    // $('.btn-danger').click(function (evt) {
-    //     response = window.confirm("Are You Sure You Want To Do This");
-    //     if (!response) {
-    //       evt.preventDefault();
-    //     }
-    //   });
+    $('.btn-danger').click(function (evt) {
+        response = window.confirm("Are You Sure You Want To Do This");
+        if (!response) {
+          evt.preventDefault();
+        }
+      });
+
+      if($('.admin-menu')){
+        $('.nk-content').css('min-height', `${window.innerHeight + 10}px`);
+      }else{
+        $('.nk-content').css('min-height', `${window.innerHeight - 62}px`);
+      }
+
 
   const presentMoney = (money) => {
     if(money){
